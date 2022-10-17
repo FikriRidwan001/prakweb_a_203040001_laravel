0cedd4f4467ed235cdff5bd6f7da1d8419a2e901
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-10-17 18:03:54 +0700

    menyelesaikan video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Fikri",
+        "email" => "203040001@mail.unpas.ac.id",
+        "image" => "Profil Fikri.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
