cb0013230c7f1c036e45320956b68dbdc123fb76
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-10-25 23:49:42 +0700

    menyelesaikan video 9 : Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +19,62 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Fikri Ridwan',
+            'email' => 'fikri@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Doddy',
+            'email' => 'doddy@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lo3em ipsum dolor sit amet consectetur, adipisicing elit. Nisi, possimus?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at dolor dignissimos minus sed, eum dolores, excepturi a itaque porro obcaecati aliquam totam? Ipsum provident autem rem mollitia aspernatur rerum dolores laudantium dicta, accusantium facilis eligendi odio minima delectus iusto laboriosam quas, quae commodi maiores cumque molestiae accusamus praesentium possimus! Dolorem, adipisci? Sunt velit qui ullam, fugit minus in nemo numquam, vel, autem fugiat magnam? Harum nulla et, ipsam at dignissimos quidem ab culpa quis perferendis deleniti rerum reiciendis eum in repudiandae quo velit explicabo minus nihil autem aperiam non. Nulla pariatur repellat aut voluptatem nam. Tempore porro voluptates accusantium!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lo3em ipsum dolor sit amet consectetur, adipisicing elit. Nisi, possimus?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at dolor dignissimos minus sed, eum dolores, excepturi a itaque porro obcaecati aliquam totam? Ipsum provident autem rem mollitia aspernatur rerum dolores laudantium dicta, accusantium facilis eligendi odio minima delectus iusto laboriosam quas, quae commodi maiores cumque molestiae accusamus praesentium possimus! Dolorem, adipisci? Sunt velit qui ullam, fugit minus in nemo numquam, vel, autem fugiat magnam? Harum nulla et, ipsam at dignissimos quidem ab culpa quis perferendis deleniti rerum reiciendis eum in repudiandae quo velit explicabo minus nihil autem aperiam non. Nulla pariatur repellat aut voluptatem nam. Tempore porro voluptates accusantium!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lo3em ipsum dolor sit amet consectetur, adipisicing elit. Nisi, possimus?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at dolor dignissimos minus sed, eum dolores, excepturi a itaque porro obcaecati aliquam totam? Ipsum provident autem rem mollitia aspernatur rerum dolores laudantium dicta, accusantium facilis eligendi odio minima delectus iusto laboriosam quas, quae commodi maiores cumque molestiae accusamus praesentium possimus! Dolorem, adipisci? Sunt velit qui ullam, fugit minus in nemo numquam, vel, autem fugiat magnam? Harum nulla et, ipsam at dignissimos quidem ab culpa quis perferendis deleniti rerum reiciendis eum in repudiandae quo velit explicabo minus nihil autem aperiam non. Nulla pariatur repellat aut voluptatem nam. Tempore porro voluptates accusantium!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lo3em ipsum dolor sit amet consectetur, adipisicing elit. Nisi, possimus?',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at dolor dignissimos minus sed, eum dolores, excepturi a itaque porro obcaecati aliquam totam? Ipsum provident autem rem mollitia aspernatur rerum dolores laudantium dicta, accusantium facilis eligendi odio minima delectus iusto laboriosam quas, quae commodi maiores cumque molestiae accusamus praesentium possimus! Dolorem, adipisci? Sunt velit qui ullam, fugit minus in nemo numquam, vel, autem fugiat magnam? Harum nulla et, ipsam at dignissimos quidem ab culpa quis perferendis deleniti rerum reiciendis eum in repudiandae quo velit explicabo minus nihil autem aperiam non. Nulla pariatur repellat aut voluptatem nam. Tempore porro voluptates accusantium!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
