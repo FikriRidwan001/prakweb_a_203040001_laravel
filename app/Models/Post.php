60d5a107892a14195a62d9052c39708305a63aab
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-10-25 21:27:35 +0700

    menyelesaikan video 7 : Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,31 +2,14 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fikri Ridwan Faturohman",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque repellendus quisquam cupiditate maxime laboriosam quia voluptate quasi at veritatis accusamus, fugiat eius harum iste voluptatem nam totam? Possimus, eius deleniti. Non, impedit itaque praesentium obcaecati mollitia expedita, eum possimus debitis temporibus, dolores laudantium amet cumque accusamus explicabo repudiandae consequuntur dolor distinctio rem error doloremque natus atque voluptatibus totam. Dolor cumque vero illum tenetur facere mollitia natus, cum perspiciatis! Optio, illum dignissimos consequatur temporibus accusamus adipisci exercitationem magnam nemo error id?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis accusantium placeat quae laboriosam nobis, soluta, aliquam reiciendis, mollitia neque natus modi porro perspiciatis repudiandae at aperiam ad ratione officiis minus? Atque tenetur doloremque non, eos quidem aut aperiam saepe perspiciatis laboriosam totam nulla iusto reiciendis dolorem aliquid quam ex, laudantium quo odio tempora odit deserunt ratione? Nobis esse neque molestiae consequatur eum commodi dolorum maiores autem eaque vitae natus corrupti iste illum, libero ullam eligendi ex quaerat. Dicta nemo sed velit esse in sapiente voluptatibus quibusdam repellendus, ut, odit sint ratione omnis possimus doloremque consequuntur quod soluta atque doloribus modi?"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
 }
